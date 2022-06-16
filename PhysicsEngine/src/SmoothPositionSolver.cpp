247f77d0270d25a0c0b67eb13e363138b3ee50c2
Stowy <stowy@protonmail.ch>
2022-06-16 19:13:59 +0200

    finished smooth position solver

--- PhysicsEngine/src/SmoothPositionSolver.cpp
+++ PhysicsEngine/src/SmoothPositionSolver.cpp
@@ -2,48 +2,42 @@
 
 #include <vector>
 
-#include "Solver.hpp"
 #include "Rigidbody.hpp"
 #include "VecUtils.hpp"
 
 void SmoothPositionSolver::Solve(const std::vector<Collision>& collisions, float deltaTime)
 {
-	std::vector<std::pair<sf::Vector2f, sf::Vector2f>> deltas;
+	//std::vector<std::pair<sf::Vector2f, sf::Vector2f>> deltas;
 
-	for (const Collision& collision : collisions)
+	for (const auto& [bodyA, bodyB, points] : collisions)
 	{
 		// ReSharper disable CppCStyleCast
-		Rigidbody* aBody = collision.bodyA->IsDynamic() ? (Rigidbody*)collision.bodyA : nullptr;
-		Rigidbody* bBody = collision.bodyB->IsDynamic() ? (Rigidbody*)collision.bodyB : nullptr;
+		Rigidbody* aBody = bodyA->IsDynamic() ? (Rigidbody*)bodyA : nullptr;
+		Rigidbody* bBody = bodyB->IsDynamic() ? (Rigidbody*)bodyB : nullptr;
 		// ReSharper restore CppCStyleCast
 
 		const float aInvMass = aBody ? aBody->InvMass() : 0.0f;
 		const float bInvMass = bBody ? bBody->InvMass() : 0.0f;
 
-		sf::Vector2f resolution = collision.points.b - collision.points.a;
+		sf::Vector2f resolution = points.b - points.a;
 
 		constexpr float percent = 0.8f;
 		constexpr float slop = 0.01f;
 
-		sf::Vector2f correction = collision.points.normal * percent
+		sf::Vector2f correction = points.normal * percent
 			* std::fmax(Magnitude(resolution) - slop, 0.0f)
 			/ (aInvMass + bInvMass);
 
-		sf::Vector2f deltaA;
-		sf::Vector2f deltaB;
-
 		if (aBody ? aBody->IsKinematic() : false)
 		{
-			deltaA = -aInvMass * correction;
+			const sf::Vector2f deltaA = -aInvMass * correction;
+			aBody->Trans()->position += deltaA;
 		}
 
 		if (bBody ? bBody->IsKinematic() : false)
 		{
-			deltaB = bInvMass * correction;
+			const sf::Vector2f deltaB = bInvMass * correction;
+			bBody->Trans()->position += deltaB;
 		}
-
-		deltas.emplace_back(deltaA, deltaB);
 	}
-
-	// TODO : Continue line 43
 }
