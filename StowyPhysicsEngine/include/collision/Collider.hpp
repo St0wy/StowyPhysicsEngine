040674f2edaa4d4de4a0791badceda4174299279
Stowy <stowy@protonmail.ch>
2023-06-06 18:38:33 +0200

    Added constructor to CircleCollider

--- StowyPhysicsEngine/include/collision/Collider.hpp
+++ StowyPhysicsEngine/include/collision/Collider.hpp
@@ -29,87 +29,72 @@ class AabbCollider;
  */
 class Collider
 {
-public:
-	Collider() = default;
-	Collider(Collider&& col) noexcept = default;
-	virtual ~Collider() = default;
-	Collider& operator=(Collider&& col) = default;
-	Collider& operator=(const Collider& col) = default;
-	Collider(const Collider& col) = default;
+  public:
+    Collider() = default;
+    Collider(Collider &&col) noexcept = default;
+    virtual ~Collider() = default;
+    Collider &operator=(Collider &&col) = default;
+    Collider &operator=(const Collider &col) = default;
+    Collider(const Collider &col) = default;
 
-	/**
-	* \brief The center of the collider.
-	*/
-	Vector2 center{};
+    /**
+     * \brief The center of the collider.
+     */
+    Vector2 center{};
 
-	/**
-	 * \brief Tests the collision against a generic collider.
-	 * \param transform The transform of this collider.
-	 * \param collider The collider to collide with.
-	 * \param colliderTransform The transform of the collider to collide with.
-	 * \return The manifold of that collision.
-	 */
-	virtual Manifold TestCollision(
-		const Transform* transform,
-		const Collider* collider,
-		const Transform* colliderTransform
-	) const = 0;
+    /**
+     * \brief Tests the collision against a generic collider.
+     * \param transform The transform of this collider.
+     * \param collider The collider to collide with.
+     * \param colliderTransform The transform of the collider to collide with.
+     * \return The manifold of that collision.
+     */
+    virtual Manifold TestCollision(const Transform *transform, const Collider *collider,
+                                   const Transform *colliderTransform) const = 0;
 
-	/**
-	 * \brief Tests the collision against a box collider.
-	 * \param transform The transform of this collider.
-	 * \param collider The box collider to collide with.
-	 * \param boxTransform The transform of the collider to collide with.
-	 * \return The manifold of that collision.
-	 */
-	virtual Manifold TestCollision(
-		const Transform* transform,
-		const BoxCollider* collider,
-		const Transform* boxTransform
-	) const = 0;
+    /**
+     * \brief Tests the collision against a box collider.
+     * \param transform The transform of this collider.
+     * \param collider The box collider to collide with.
+     * \param boxTransform The transform of the collider to collide with.
+     * \return The manifold of that collision.
+     */
+    virtual Manifold TestCollision(const Transform *transform, const BoxCollider *collider,
+                                   const Transform *boxTransform) const = 0;
 
-	/**
-	 * \brief Tests the collision against a circle collider.
-	 * \param transform The transform of this collider.
-	 * \param collider The circle collider to collide with.
-	 * \param circleTransform The transform of the collider to collide with.
-	 * \return The manifold of that collision.
-	 */
-	virtual Manifold TestCollision(
-		const Transform* transform,
-		const CircleCollider* collider,
-		const Transform* circleTransform
-	) const = 0;
+    /**
+     * \brief Tests the collision against a circle collider.
+     * \param transform The transform of this collider.
+     * \param collider The circle collider to collide with.
+     * \param circleTransform The transform of the collider to collide with.
+     * \return The manifold of that collision.
+     */
+    virtual Manifold TestCollision(const Transform *transform, const CircleCollider *collider,
+                                   const Transform *circleTransform) const = 0;
 
-	/**
-	 * \brief Tests the collision against a aabb collider.
-	 * \param transform The transform of this collider.
-	 * \param collider The circle collider to collide with.
-	 * \param aabbTransform The transform of the collider to collide with.
-	 * \return The manifold of that collision.
-	 */
-	virtual Manifold TestCollision(
-		const Transform* transform,
-		const AabbCollider* collider,
-		const Transform* aabbTransform
-	) const = 0;
+    /**
+     * \brief Tests the collision against a aabb collider.
+     * \param transform The transform of this collider.
+     * \param collider The circle collider to collide with.
+     * \param aabbTransform The transform of the collider to collide with.
+     * \return The manifold of that collision.
+     */
+    virtual Manifold TestCollision(const Transform *transform, const AabbCollider *collider,
+                                   const Transform *aabbTransform) const = 0;
 
-	/**
-	 * \brief Find the furthest point in the specified direction.
-	 * \param transform The transform of this collider.
-	 * \param direction Direction in which to find the furthest point.
-	 * \return The furthest point.
-	 */
-	[[nodiscard]] virtual Vector2 FindFurthestPoint(
-		const Transform* transform,
-		const Vector2& direction
-	) const = 0;
+    /**
+     * \brief Find the furthest point in the specified direction.
+     * \param transform The transform of this collider.
+     * \param direction Direction in which to find the furthest point.
+     * \return The furthest point.
+     */
+    [[nodiscard]] virtual Vector2 FindFurthestPoint(const Transform *transform, const Vector2 &direction) const = 0;
 
-	/**
-	 * \brief Gets the size of the box that surrounds the collider.
-	 * \return The bounding box of the collider.
-	 */
-	[[nodiscard]] virtual Vector2 GetBoundingBoxSize() const = 0;
+    /**
+     * \brief Gets the size of the box that surrounds the collider.
+     * \return The bounding box of the collider.
+     */
+    [[nodiscard]] virtual Vector2 GetBoundingBoxSize() const = 0;
 };
 
 /**
@@ -117,61 +102,47 @@ public:
  */
 class BoxCollider final : public Collider
 {
-public:
-	/**
-	 * \brief Half of the width of the box.
-	 */
-	float halfWidth = 0;
-	/**
-	 * \brief Half of the height of the box.
-	 */
-	float halfHeight = 0;
+  public:
+    /**
+     * \brief Half of the width of the box.
+     */
+    float halfWidth = 0;
+    /**
+     * \brief Half of the height of the box.
+     */
+    float halfHeight = 0;
 
-	Manifold TestCollision(
-		const Transform* transform,
-		const Collider* collider,
-		const Transform* colliderTransform
-	) const override;
+    Manifold TestCollision(const Transform *transform, const Collider *collider,
+                           const Transform *colliderTransform) const override;
 
-	Manifold TestCollision(
-		const Transform* transform,
-		const BoxCollider* collider,
-		const Transform* boxTransform
-	) const override;
+    Manifold TestCollision(const Transform *transform, const BoxCollider *collider,
+                           const Transform *boxTransform) const override;
 
-	Manifold TestCollision(
-		const Transform* transform,
-		const CircleCollider* collider,
-		const Transform* circleTransform
-	) const override;
+    Manifold TestCollision(const Transform *transform, const CircleCollider *collider,
+                           const Transform *circleTransform) const override;
 
-	Manifold TestCollision(const Transform* transform, const AabbCollider* collider,
-		const Transform* circleTransform) const override;
+    Manifold TestCollision(const Transform *transform, const AabbCollider *collider,
+                           const Transform *circleTransform) const override;
 
-	[[nodiscard]] Vector2 FindFurthestPoint(
-		const Transform* transform,
-		const Vector2& direction
-	) const override;
+    [[nodiscard]] Vector2 FindFurthestPoint(const Transform *transform, const Vector2 &direction) const override;
 
-	/**
-	 * \brief Gets the transformed vertices of the box.
-	 * \param transform The transform to apply to the collider.
-	 * \return The transform vertices of the box.
-	 */
-	[[nodiscard]] std::array<Vector2, 4> GetTransformedVertices(const Transform& transform) const;
+    /**
+     * \brief Gets the transformed vertices of the box.
+     * \param transform The transform to apply to the collider.
+     * \return The transform vertices of the box.
+     */
+    [[nodiscard]] std::array<Vector2, 4> GetTransformedVertices(const Transform &transform) const;
 
-	/**
-	 * \brief Gets the untransformed vertices of the box.
-	 * \return The vertices of the box.
-	 */
-	[[nodiscard]] std::array<Vector2, 4> GetVertices() const;
+    /**
+     * \brief Gets the untransformed vertices of the box.
+     * \return The vertices of the box.
+     */
+    [[nodiscard]] std::array<Vector2, 4> GetVertices() const;
 
-	[[nodiscard]] static Projection Project(
-		const Vector2& axis,
-		const std::array<Vector2, 4>& vertices);
-	[[nodiscard]] static std::array<Vector2, 4> GetAxes(const std::array<Vector2, 4>& vertices);
+    [[nodiscard]] static Projection Project(const Vector2 &axis, const std::array<Vector2, 4> &vertices);
+    [[nodiscard]] static std::array<Vector2, 4> GetAxes(const std::array<Vector2, 4> &vertices);
 
-	[[nodiscard]] Vector2 GetBoundingBoxSize() const override;
+    [[nodiscard]] Vector2 GetBoundingBoxSize() const override;
 };
 
 /**
@@ -179,38 +150,29 @@ public:
  */
 class CircleCollider final : public Collider
 {
-public:
-	/**
-	 * \brief Radius of the circle.
-	 */
-	float radius = 0;
+  public:
+    /**
+     * \brief Radius of the circle.
+     */
+    float radius = 0;
 
-	Manifold TestCollision(
-		const Transform* transform,
-		const Collider* collider,
-		const Transform* colliderTransform
-	) const override;
+    CircleCollider() = default;
+    CircleCollider(float radius);
 
-	Manifold TestCollision(
-		const Transform* transform,
-		const BoxCollider* collider,
-		const Transform* boxTransform
-	) const override;
+    Manifold TestCollision(const Transform *transform, const Collider *collider,
+                           const Transform *colliderTransform) const override;
 
-	Manifold TestCollision(
-		const Transform* transform,
-		const CircleCollider* collider,
-		const Transform* circleTransform
-	) const override;
+    Manifold TestCollision(const Transform *transform, const BoxCollider *collider,
+                           const Transform *boxTransform) const override;
 
-	Manifold TestCollision(const Transform* transform, const AabbCollider* collider,
-		const Transform* aabbTransform) const override;
+    Manifold TestCollision(const Transform *transform, const CircleCollider *collider,
+                           const Transform *circleTransform) const override;
 
-	[[nodiscard]] Vector2 FindFurthestPoint(
-		const Transform* transform,
-		const Vector2& direction
-	) const override;
-	[[nodiscard]] Vector2 GetBoundingBoxSize() const override;
+    Manifold TestCollision(const Transform *transform, const AabbCollider *collider,
+                           const Transform *aabbTransform) const override;
+
+    [[nodiscard]] Vector2 FindFurthestPoint(const Transform *transform, const Vector2 &direction) const override;
+    [[nodiscard]] Vector2 GetBoundingBoxSize() const override;
 };
 
 /**
@@ -218,25 +180,25 @@ public:
  */
 class AabbCollider final : public Collider
 {
-public:
-	/**
-	 * \brief Half of the width of the box.
-	 */
-	float halfWidth = 0;
-	/**
-	 * \brief Half of the height of the box.
-	 */
-	float halfHeight = 0;
+  public:
+    /**
+     * \brief Half of the width of the box.
+     */
+    float halfWidth = 0;
+    /**
+     * \brief Half of the height of the box.
+     */
+    float halfHeight = 0;
 
-	Manifold TestCollision(const Transform* transform, const Collider* collider,
-		const Transform* colliderTransform) const override;
-	Manifold TestCollision(const Transform* transform, const BoxCollider* collider,
-		const Transform* boxTransform) const override;
-	Manifold TestCollision(const Transform* transform, const CircleCollider* collider,
-		const Transform* circleTransform) const override;
-	Manifold TestCollision(const Transform* transform, const AabbCollider* collider,
-		const Transform* aabbTransform) const override;
-	[[nodiscard]] Vector2 FindFurthestPoint(const Transform* transform, const Vector2& direction) const override;
-	[[nodiscard]] Vector2 GetBoundingBoxSize() const override;
+    Manifold TestCollision(const Transform *transform, const Collider *collider,
+                           const Transform *colliderTransform) const override;
+    Manifold TestCollision(const Transform *transform, const BoxCollider *collider,
+                           const Transform *boxTransform) const override;
+    Manifold TestCollision(const Transform *transform, const CircleCollider *collider,
+                           const Transform *circleTransform) const override;
+    Manifold TestCollision(const Transform *transform, const AabbCollider *collider,
+                           const Transform *aabbTransform) const override;
+    [[nodiscard]] Vector2 FindFurthestPoint(const Transform *transform, const Vector2 &direction) const override;
+    [[nodiscard]] Vector2 GetBoundingBoxSize() const override;
 };
-}
+} // namespace stw
